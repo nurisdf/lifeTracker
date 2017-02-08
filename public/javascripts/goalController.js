f1419d14f608749c3d9f01727b33e779ad942c42
Jesús Martínez <profejesusmartinez@gmail.com>
2017-02-09 00:07:30 +0100

    OK and KO fields added to the DB schema
    OK and KO fields are used to save the total number of
    successful/unsuccessful days for each goal.
    
    - The fields have been added to the database schema along with methods
    to update them
    - Two new routes (PUT /goals/:goal/[ok|ko]) added to the server
    - Angular GoalService and Controller modified in order to call the
    routes

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -1,15 +1,22 @@
 angular.module('goalController', [])
 .controller('goalCtrl', function($scope, $http, Goals) {
   Goals.get().success(function(data) {
+    for (var currentIndex=0; currentIndex<data.length; currentIndex=currentIndex+1) {
+      var currentElement = data[currentIndex];
+      currentElement.percentage = (currentElement.ok/(currentElement.ok+currentElement.ko))*100;
+    }
     $scope.goals = data;
   });
   $scope.addGoal = function() {
     if(!$scope.goalName || $scope.goalName === '') { return; }
     var goalObject = {
-      description: $scope.goalName
+      description: $scope.goalName,
+      ok: 0,
+      ko: 0
     };
     Goals.create(goalObject).success(function(data) {
-      $scope.goals.push(goalObject);
+      data.percentage = 0;
+      $scope.goals.push(data);
       $scope.goalName = '';
     });
   };
@@ -17,6 +24,14 @@ angular.module('goalController', [])
     Goals.delete(goal._id).success(function(data) {
       var index = $scope.goals.indexOf(goal);
       $scope.goals.splice(index, 1);
+    });
+  };
+  $scope.addOk = function(goal) {
+    Goals.addOk(goal._id).success(function(data) {
+    });
+  };
+  $scope.addKo = function(goal) {
+    Goals.addKo(goal._id).success(function(data) {
     });
   };
 });
